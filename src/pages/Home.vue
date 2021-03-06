dcb90d4850370d145c926b757119ba8c68de6026
Andhika Nugraha <andhika.nugraha@go-jek.com>
2021-03-06 20:43:24 +0800

    MVP

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-12">
+    <h1 class="text-4xl font-semibold mb-4">kodewilayah<span class="text-red-500">.id</span></h1>
+    <p class="mb-8 text-gray-500 text-xl">A curated reference of Indonesian region codes</p>
+    <p class="mb-8">
+      <a href="/data/dagri2017.csv" class="px-6 py-3 inline-block bg-red-500 rounded-lg text-white font-semibold">Download CSV</a>
+      <!-- <a href="#/browse" class="px-6 py-3 inline-block bg-gray-100 text-gray-600 rounded-lg ml-4 border-gray-200 border">Browse hierarchy</a> -->
+    </p>
+    <hr class="mb-8" />
+    <h2 class="mb-4 text-gray-600 font-semibold">Dataset roadmap</h2>
+    <ul class="mb-4 text-gray-600 list-disc ml-5">
+      <li class="mb-2">Permendagri 137/2017: Provinsi, Kabupaten/Kota, Kecamatan, Kelurahan/Desa</li>
+      <li class="mb-2 text-gray-400">Postal codes</li>
+      <li class="mb-2 text-gray-400">BPS</li>
+      <li class="mb-2 text-gray-400">SLIK OJK (POJK 18/POJK.03/2017): Provinsi, Kabupaten/Kota</li>
+      <li class="mb-2 text-gray-400">Pusdafil OJK</li>
+    </ul>
+  </div>
+</template>
