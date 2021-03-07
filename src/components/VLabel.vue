5e673f41282073d81681e9fa12823698b4826db1
Andhika Nugraha <andhika.nugraha@go-jek.com>
2021-03-07 16:59:09 +0800

    Sanitised dagri CSV

---
+++ src/components/VLabel.vue
@@ -0,0 +1,5 @@
+<template>
+    <span class="text-xs uppercase bg-yellow-50 text-yellow-600 ml-1 relative -top-0.25 px-1 py-0.25 rounded inline-block">
+        <slot></slot>
+    </span>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="mt-12">
-    <h1 class="text-4xl font-semibold mb-4">kodewilayah<span class="text-red-500">.id</span></h1>
-    <p class="mb-8 text-gray-500 text-xl">A curated reference of Indonesian region codes</p>
+  <div class="mt-6 md:lt-12 lg:mt-24">
+    <h1 class="text-4xl font-semibold mb-6">kodewilayah<span class="text-red-500">.id</span></h1>
+    <p class="mb-4 text-gray-500 text-xl">Referensi terpilih kode wilayah se-Indonesia, hingga tingkat kelurahan/desa</p>
     <p class="mb-8">
-      <a href="/data/dagri2017.csv" class="px-6 py-3 inline-block bg-red-500 rounded-lg text-white font-semibold">Download CSV</a>
+      <a href="/data/dagri2017.csv" class="px-6 py-3 inline-block bg-red-500 rounded-lg text-white font-semibold">Unduh CSV</a>
       <!-- <a href="#/browse" class="px-6 py-3 inline-block bg-gray-100 text-gray-600 rounded-lg ml-4 border-gray-200 border">Browse hierarchy</a> -->
     </p>
     <hr class="mb-8" />
-    <h2 class="mb-4 text-gray-600 font-semibold">Dataset roadmap</h2>
+    <h2 class="mb-2 text-gray-800 font-semibold">Sumber data</h2>
     <ul class="mb-4 text-gray-600 list-disc ml-5">
-      <li class="mb-2">Permendagri 137/2017: Provinsi, Kabupaten/Kota, Kecamatan, Kelurahan/Desa</li>
-      <li class="mb-2 text-gray-400">Postal codes</li>
-      <li class="mb-2 text-gray-400">BPS</li>
-      <li class="mb-2 text-gray-400">SLIK OJK (POJK 18/POJK.03/2017): Provinsi, Kabupaten/Kota</li>
-      <li class="mb-2 text-gray-400">Pusdafil OJK</li>
+      <li class="mb-1">Permendagri 137/2017 (Dukcapil)</li>
+      <li class="mb-1">Kode pos <v-label>Coming soon</v-label></li>
+      <li class="mb-1">BPS <v-label>Coming soon</v-label></li>
+      <li class="mb-1">SLIK OJK (POJK 18/POJK.03/2017) <v-label>Coming soon</v-label></li>
+      <li class="mb-1">Pusdafil OJK <v-label>Coming soon</v-label></li>
     </ul>
   </div>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import VLabel from '../components/VLabel.vue';
+  export default defineComponent({
+    components: { 'v-label': VLabel }
+  })
+</script>
